f9697af4b5864d5f1d5be195aab63ff998789f1f
Christian <chrisaug@rogers.com>
2020-03-02 16:39:46 -0500

    added avg times for fcfs

--- schedule_fcfs.c
+++ schedule_fcfs.c
@@ -8,32 +8,39 @@
 
 struct node *head = NULL;
 struct node *node = NULL;
-// struct node *tail = NULL;
+int processes = 0;
+float completion_time = 0, waittime = 0;
+float avg_turnaround = 0;
+float avg_wait = 0;
+float avg_response = 0;
 
 void add(char *name, int priority, int burst) //adds a new task to the list
 {
+    processes++;
+
     Task *task = malloc(sizeof(struct node)); //creates a new task struct
     //assigns all the attributes
     task->name = name;
     task->priority = priority;
     task->burst = burst;
 
-    if (head == NULL)//if the list has not been created
+    if (head == NULL) //if the list has not been created
     {
-        head = malloc(sizeof(struct node));//allocate size for the head node
-        head->task = task; //assign task to the task attribute in head
+        head = malloc(sizeof(struct node)); //allocate size for the head node
+        head->task = task;                  //assign task to the task attribute in head
     }
-    else 
+    else
     {
-        insert(&head, task);//insert the new node as the head and give a link to the previous head
+        insert(&head, task); //insert the new node as the head and give a link to the previous head
     }
 }
 
-Task *pickNextTask() {
+Task *pickNextTask()
+{
     Task *curr_task = head->task;
     struct node *next_task = head->next;
 
-    while(next_task != NULL)
+    while (next_task != NULL)
     {
         curr_task = next_task->task; //assigns the current task to be the next nodes task
         next_task = next_task->next; //makes the next task the pointer to the next node in the struct
@@ -46,11 +53,18 @@ void schedule()
 {
     Task *current_task;
 
-    while(head != NULL)
+    while (head != NULL)
     {
-        current_task = pickNextTask(); //find the first task in the linked list
+        current_task = pickNextTask();          //find the first task in the linked list
         run(current_task, current_task->burst); //runs it
-        delete(&head, current_task); //removes the tail from the linked list 
-    }
-}
 
+        completion_time += current_task->burst;
+        waittime = completion_time - current_task->burst;
+        
+        delete (&head, current_task);           //removes the tail from the linked list
+    }
+    avg_turnaround = completion_time / processes;
+    avg_wait = waittime / processes;
+    avg_response = avg_wait;
+    printf("Average Turnaround Time: %.2f\nAverage Waiting Time: %.2f\nAverage Response Time: %.2f\n", avg_turnaround, avg_wait, avg_response);
+}
