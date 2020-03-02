918165d216ad748f2a28e441ce24e3ba0630681e
Christian <chrisaug@rogers.com>
2020-03-02 18:25:53 -0500

    added avg times for fcfs, sjf, and priority

--- schedule_sjf.c
+++ schedule_sjf.c
@@ -9,9 +9,19 @@
 struct node *head = NULL;
 struct node *node = NULL;
 // struct node *tail = NULL;
+float avg_turnaround;
+float avg_wait;
+float avg_response;
+
+int processes;
+int time;
+int completion_time;
+int waittime;
 
 void add(char *name, int priority, int burst) //adds a new task to the list
 {
+    processes += 1;
+
     Task *task = malloc(sizeof(struct node)); //creates a new task struct
     //assigns all the attributes
     task->name = name;
@@ -58,6 +68,14 @@ void schedule()
     {
         current_task = pickNextTask();
         run(current_task, current_task->burst);
+
+        completion_time += current_task->burst;
+        waittime = completion_time - current_task->burst;
+
         delete (&head, current_task);
     }
+    avg_turnaround = completion_time / processes;
+    avg_wait = waittime / processes;
+    avg_response = avg_wait;
+    printf("Average Turnaround Time: %.2f\nAverage Waiting Time: %.2f\nAverage Response Time: %.2f\n", avg_turnaround, avg_wait, avg_response);
 }
